409e40f31d3c7deadae0a51a8899e9b20bf714eb
thommorais <thommorais@gmail.com>
2025-05-28 19:23:40 -0300

    chore: adding playwright to test the UI

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,8 +1,8 @@
+import { cleanup } from '@testing-library/react'
 // Vitest setup file
 import { beforeEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
 
 // Clean up after each test
 beforeEach(() => {
-  cleanup()
+	cleanup()
 })
