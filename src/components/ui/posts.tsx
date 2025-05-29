4623739a1cb45b7895733ff909f1e855724e034b
thommorais <thommorais@gmail.com>
2025-05-29 02:54:30 -0300

    chore: add suspense wrapper to react

--- src/components/ui/posts.tsx
+++ src/components/ui/posts.tsx
@@ -1,15 +1,22 @@
 import { PaginationWrapper } from '_/components/ui/posts-pagination'
 import { PostsSearch } from '_/components/ui/posts-search'
 import { TableWrapper } from '_/components/ui/posts-table'
+import { Suspense } from 'react'
 
 const Posts = () => {
 	return (
 		<section className='mx-auto my-12 flex w-screen max-w-6xl grow flex-col p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-xs lg:ring-1 lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10'>
 			<header className='mb-10'>
-				<PostsSearch />
+				<Suspense>
+					<PostsSearch />
+				</Suspense>
 			</header>
-			<TableWrapper />
-			<PaginationWrapper />
+			<Suspense>
+				<TableWrapper />
+			</Suspense>
+			<Suspense>
+				<PaginationWrapper />
+			</Suspense>
 		</section>
 	)
 }
